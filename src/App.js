5c7168ed3403686ce6150d1fcf60dd2153960bd8
maytezhou <you@example.com>
2019-07-20 18:18:19 -0500

    agregando un spinner,fetching users with axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React,{Component} from 'react';
 import './App.css';
 import Users from './components/users/Users'
+import axios from 'axios';
 import Navbar from './components/layouts/Navbar'
 
 class App extends Component{
+  state = {
+    users:[],
+    loading:false,
+  }
+   async componentDidMount(){
+ this.setState({loading:true});
+    const res = await axios.get('https://api.github.com/users')
+this.setState({users:res.data,loading:false});
 
-  render(){
-  
-   
- /*  if(loading){
-     return <h4>Loading</h4>
-   }*/
+
+  }
+
+  render(){  
+
     return (
       <div className="App">        
        <Navbar/>
        <div className="container">
-       <Users/>
+       <Users loading={this.state.loading} users={this.state.users}/>
        </div>
     
       </div>
